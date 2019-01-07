b3b20e18845f84b0b84ee1e6644bb936f5d7b429
Darek Frynas <dfrynas@apptension.com>
2019-01-07 09:51:42 +0100

    Log errors to sentry

---
+++ src/components/SentryBoundary/SentryBoundary.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as Sentry from '@sentry/browser';
+
+export class SentryBoundary extends Component {
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+      Sentry.captureException(error);
+    });
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
