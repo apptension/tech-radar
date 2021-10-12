9bc7a6701228e563582013a0688576b73d341d4a
MajaZubko <majamzubko@gmail.com>
2021-10-12 15:19:48 +0200

    Create mock list component

---
+++ src/shared/components/list/list.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from './list.styles';
+
+export const List = () => {
+  return <Container>List</Container>;
+};
---
+++ src/shared/components/list/list.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: red;
+  height: 100vh;
+  width: 411px;
+`;
