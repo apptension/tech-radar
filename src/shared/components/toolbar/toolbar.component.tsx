51d9071c26ad623aec473637d8be52f73cb1c53a
MajaZubko <majamzubko@gmail.com>
2021-10-12 15:20:07 +0200

    Create mock toolbar component

---
+++ src/shared/components/toolbar/toolbar.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from './toolbar.styles';
+
+export const Toolbar = () => {
+  return <Container>Hello</Container>;
+};
---
+++ src/shared/components/toolbar/toolbar.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: green;
+  height: 60px;
+  width: 697px;
+  position: fixed;
+  bottom: 24px;
+  right: 340px;
+`;
