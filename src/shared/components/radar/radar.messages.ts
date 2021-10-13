8551a6742be2e928a4eb0e8691624c9f8903f874
MajaZubko <majamzubko@gmail.com>
2021-10-13 09:02:56 +0200

    Add messages to radar directory

---
+++ src/shared/components/radar/radar.messages.ts
@@ -0,0 +1,40 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  title: {
+    id: 'radar.title',
+    defaultMessage: 'Tech Radar',
+  },
+  innerCircle: {
+    id: 'radar.innerCircle',
+    defaultMessage: 'Adopt',
+  },
+  secondCircle: {
+    id: 'radar.secondCircle',
+    defaultMessage: 'Trial',
+  },
+  thirdCircle: {
+    id: 'radar.thirdCircle',
+    defaultMessage: 'Assess',
+  },
+  outerCircle: {
+    id: 'radar.outerCircle',
+    defaultMessage: 'Hold',
+  },
+  infrastructureLabel: {
+    id: 'radar.infrastructureLabel',
+    defaultMessage: 'Infrastructure',
+  },
+  languagesFrameworksLabel: {
+    id: 'radar.languagesFrameworksLabel',
+    defaultMessage: 'Languages & Frameworks',
+  },
+  packagesLibrariesLabel: {
+    id: 'radar.packagesLibrariesLabel',
+    defaultMessage: 'Packages & Libraries',
+  },
+  productsToolsLabel: {
+    id: 'radar.productsToolsLabel',
+    defaultMessage: 'Products & Tools',
+  },
+});
