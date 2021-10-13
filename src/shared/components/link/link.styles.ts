10b90d2a9cdf8c47580b6e7ba34e87e053e639a4
Przemyslaw <pkwiatkowski@apptension.com>
2021-10-13 16:56:09 +0200

    Handle buttons variants

---
+++ src/shared/components/link/link.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const OuterLink = styled.a`
+  text-decoration: none;
+`;
+
+export const InnerLink = styled(Link)`
+  text-decoration: none;
+`;
