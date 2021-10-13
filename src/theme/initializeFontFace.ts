1ffa2552451e269eaf76c11fed76d540d7a27db1
Przemyslaw <pkwiatkowski@apptension.com>
2021-10-13 15:48:21 +0200

    Add fonts

--- src/theme/initializeFontFace.ts
+++ src/theme/initializeFontFace.ts
@@ -1,34 +1,21 @@
-/* eslint-disable import/no-dynamic-require */
-import { fontFamily } from './font';
-
-function fontFace(
-  name: string,
-  src: string,
-  fontWeight = 'normal',
-  fontStyle = 'normal',
-  extensions = ['woff2', 'woff', 'ttf'],
-  formats = ['woff2', 'woff', 'truetype']
-): string {
-  const sources = extensions
-    .map((ext, index) => `url(${require('../fonts/' + src + `.${ext}`)}) format("${formats[index]}")`)
-    .join(',');
-
-  return `
-    @font-face{
-        font-family: "${name}";
-        src: ${sources};
-
-        font-weight: ${fontWeight};
-        font-style: ${fontStyle};
-    }
-  `;
-}
-/* eslint-enable import/no-dynamic-require */
-
-const generateFontsString = (): string => [fontFace(fontFamily.primary, 'OpenSans')].join('\n');
+const fontFace = `
+  @font-face{
+      font-family: 'Hellix';
+      font-style: normal;
+      font-weight: normal;
+      src: url('./fonts/Hellix-Medium.otf') format("opentype");
+  }
+  
+  @font-face{
+      font-family: 'Hellix';
+      font-style: normal;
+      font-weight: bold;
+      src: url('./fonts/Hellix-SemiBold.otf') format("opentype");
+  }
+`;
 
 export default (): void => {
   const style = document.createElement('style');
-  style.innerHTML = generateFontsString();
+  style.innerHTML = fontFace;
   document.head.appendChild(style);
 };
