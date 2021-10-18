02eda3752dbdd30e60e604808b60bf6d91a8b31f
Przemyslaw <pkwiatkowski@apptension.com>
2021-10-18 10:14:31 +0200

    Display package json version

--- src/shared/components/versionTag/versionTag.component.tsx
+++ src/shared/components/versionTag/versionTag.component.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Tag } from '../tag';
 
+import { version } from '../../../../package.json';
+
 export const VersionTag = ({ ...other }) => {
-  const appVersion = process.env.REACT_APP_VERSION || 1.0;
+  const appVersion = version || 1.0;
 
   return <Tag {...other}>{appVersion}</Tag>;
 };
