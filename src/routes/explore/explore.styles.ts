dcd611877b7b2bdb98327a016486b9dede2ec493
Przemyslaw <pkwiatkowski@apptension.com>
2021-10-19 15:53:08 +0200

    Add contentful team data

--- src/routes/explore/explore.styles.ts
+++ src/routes/explore/explore.styles.ts
@@ -38,6 +38,7 @@ export const Viewer = styled.div<ViewerProps>`
   align-items: ${(props) => (props.fullRadar ? 'center' : 'flex-end')};
   justify-content: ${(props) => (props.fullRadar ? 'center' : 'flex-end')};
   width: ${(props) => (props.fullRadar ? '100%' : undefined)};
+  position: relative;
 `;
 
 export const Toolbar = styled(ToolbarComponent)`
