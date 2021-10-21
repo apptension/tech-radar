589aeffc610aaa3949afab4e42e9ae8c92b49ad8
Przemysław Kwiatkowski <44872031+appkwiatkowski@users.noreply.github.com>
2021-10-21 16:30:33 +0200

    Add shadows

--- src/shared/components/technologiesList/technologiesList.styles.ts
+++ src/shared/components/technologiesList/technologiesList.styles.ts
@@ -1,18 +1,32 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import * as colors from '../../../theme/color';
 import { Tag as TagComponent } from '../tag';
+import { color } from '../../../theme';
 
 export const ListWrapper = styled.div`
-  margin-bottom: 25px;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  overflow: auto;
-  margin-top: 16px;
+  flex: 1;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
 `;
 
-export const ListItem = styled.div`
+export const List = styled.ul`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: auto;
+  padding: 0;
+  margin: 16px 0 0;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
   margin: 10px 0;
   cursor: default;
   display: flex;
@@ -42,4 +56,27 @@ export const EmptyResults = styled.div`
   color: ${colors.boulder};
   font-size: 18px;
   margin-top: 16px;
+`;
+
+const shadowStyles = css`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 10%;
+  mix-blend-mode: darken;
+  pointer-events: none;
+`;
+
+export const ShadowBottom = styled.div<{ visible: boolean }>`
+  ${shadowStyles};
+  bottom: 0;
+  background: ${color.shadowGradientBottom};
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
+`;
+
+export const ShadowTop = styled.div<{ visible: boolean }>`
+  ${shadowStyles};
+  top: 0;
+  background: ${color.shadowGradientTop};
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
 `;
