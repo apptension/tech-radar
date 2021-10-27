7faf0faed3d45f90b4012d739dc82bee71a92338
MajaZubko <majamzubko@gmail.com>
2021-10-27 11:49:34 +0200

    Cloud animation

--- src/shared/components/background/background.component.tsx
+++ src/shared/components/background/background.component.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import CloudsImg from '../../../images/clouds.png';
-import { Container, Image } from './background.styles';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
+import { Breakpoint } from '../../../theme/media';
+import { Container, Image, MovingImage } from './background.styles';
 
 export const Background = () => {
+  const { matches: isDesktop } = useMediaQuery({ above: Breakpoint.DESKTOP });
   return (
     <Container>
-      <Image src={CloudsImg} />
+      <Image src={CloudsImg} animated={isDesktop} />
+      {isDesktop && <MovingImage src={CloudsImg} />}
     </Container>
   );
 };
