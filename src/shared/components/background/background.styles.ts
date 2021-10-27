e399e3f554dc32ca18ef67cb51e615f25d22afda
MajaZubko <66833364+MajaZubko@users.noreply.github.com>
2021-10-27 12:38:30 +0200

    Simplified clouds animation
    * Small fixes
    
    * Cloud animation

--- src/shared/components/background/background.styles.ts
+++ src/shared/components/background/background.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { zIndex } from '../../../theme';
 import { mediaQuery } from '../../../theme/media';
 
@@ -15,7 +15,30 @@ export const Container = styled.div`
   }
 `;
 
-export const Image = styled.img`
+export const firstCloudAnimation = keyframes`
+  0% {opacity: 0.15;  transform: translate(0, 0)}
+  25% {opacity: 0.2; transform: translate(-100, -400)}
+  50% {opacity: 0.15; transform: translate(-300, 600)}
+  75% {opacity: 0.1; transform: translate(-350, -300)}
+  100% {opacity: 0.15; transform: translate(0, 0)}
+`;
+
+export const secondCloudAnimation = keyframes`
+  0% {opacity: 0.1; transform: translate(0, 0)}
+  25% {opacity: 0.15; transform: translate(-100, 400)}
+  50% {opacity: 0.2; transform: translate(300, -600)}
+  75% {opacity: 0.15; transform: translate(-350, 300)}
+  100% {opacity: 0.1;  transform: translate(0, 0)}
+`;
+
+const animatedImageStyles = css`
+  transform: translate(0, 0);
+  animation-name: ${firstCloudAnimation};
+  animation-duration: 7s;
+  animation-iteration-count: infinite;
+`;
+
+export const Image = styled.img<{ animated: boolean }>`
   position: absolute;
   left: 0;
   top: 0;
@@ -24,5 +47,20 @@ export const Image = styled.img`
   object-fit: cover;
   mix-blend-mode: overlay;
   opacity: 0.15;
-  filter: blur(16px);
+  ${(props) => props.animated && animatedImageStyles}
+`;
+
+export const MovingImage = styled.img`
+  position: absolute;
+  left: 50px;
+  top: -100px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  mix-blend-mode: overlay;
+  opacity: 0.1;
+  transform: translate(0, 0) scaleX(-1);
+  animation-name: ${secondCloudAnimation};
+  animation-duration: 5s;
+  animation-iteration-count: infinite;
 `;
