a5f85f243a86afa46c447963f300e8195fdd5e23
Waldemar Fryc <79092787+wfryc-apptension@users.noreply.github.com>
2021-12-13 15:30:28 +0100

    Add 404 page styling
    Co-authored-by: wfryc <waldemar.fryc@schemadesign.com>

---
+++ src/routes/notFound/notFound.messages.ts
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  title: {
+    id: 'notFound.title',
+    defaultMessage: 'Sorry, page unavailable',
+  },
+  description: {
+    id: 'notFound.description',
+    defaultMessage: 'Something is not right :(',
+  },
+  backToHomeButton: {
+    id: 'notFound.backToHomeButton',
+    defaultMessage: 'Take me back!',
+  },
+  backToMainPageButton: {
+    id: 'notFound.backToMainPageButton',
+    defaultMessage: 'Back to apptension.com',
+  },
+});
