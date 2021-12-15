ce39d3f77422926edea8a76d62de3c0df23a07d8
wfryc <waldemar.fryc@schemadesign.com>
2021-12-15 16:33:57 +0100

    Fix typo on home desctiprion

--- src/routes/home/home.messages.ts
+++ src/routes/home/home.messages.ts
@@ -5,7 +5,7 @@ export default defineMessages({
   description: {
     id: 'home.description',
     defaultMessage:
-      'The radar will let you explore all of the technologies, methods, and tools used here at Apptension and learn more about them. You will also get to see the technologies we have on our radar (pun intened) and, hopefully, get inspired.',
+      'The radar will let you explore all of the technologies, methods, and tools used here at Apptension and learn more about them. You will also get to see the technologies we have on our radar (pun intended) and, hopefully, get inspired.',
   },
   exploreButton: {
     id: 'home.exploreButton',
