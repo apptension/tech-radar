c6000c8f6dc47300e1e4641fd4d98d4a5f76feb0
Marek Miklaszewski <mmiklaszewski@apptension.com>
2023-03-08 20:13:37 +0100

    [TRA-270] Add matrixSelectField to form, fetch seniority

--- src/shared/components/matrix/types/index.ts
+++ src/shared/components/matrix/types/index.ts
@@ -3,4 +3,12 @@ export interface PersonalInfo {
   slackId: string;
   email: string;
   name: string;
+  seniority: string;
 }
+
+export interface SelectOption {
+  value: string;
+  label: string;
+}
+
+export type Seniority = SelectOption;
--- src/shared/services/api/endpoints/airtable.ts
+++ src/shared/services/api/endpoints/airtable.ts
@@ -1,6 +1,10 @@
-import { PersonalInfo } from '../../../components/matrix/types';
+import { PersonalInfo, Seniority } from '../../../components/matrix/types';
 import { axiosAirtableApi } from '../axiosInstances';
 
 export const getUserPersonalInfo = async (userEmail: string) => {
   return await axiosAirtableApi.get<{ userInfo: PersonalInfo }>(`/getUserPersonalInfo/?email=${userEmail}`);
+};
+
+export const getSeniorities = async () => {
+  return await axiosAirtableApi.get<{ seniorities: Seniority[] }>('/getSeniorities');
 };
