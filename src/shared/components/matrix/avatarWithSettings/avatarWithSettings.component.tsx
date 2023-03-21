9cd28a81d5c8a12384db2883d031840daa53eb93
Marek Miklaszewski <mmiklaszewski@apptension.com>
2023-03-21 16:17:48 +0100

    [TRA-261] Create useOutsideClick, use it in avatar

--- src/shared/components/matrix/avatarWithSettings/avatarWithSettings.component.tsx
+++ src/shared/components/matrix/avatarWithSettings/avatarWithSettings.component.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { ReactComponent as ChevronDownSVG } from '../../../../images/icons/chevron-down.svg';
 import { useAuthContext } from '../../../../modules/auth/auth.context';
+import { useOutsideClick } from '../../../hooks/useOutsideClick/useOutsideClick.hook';
 import { Avatar, AvatarContainer, SettingsButton } from './avatarWithSettings.styles';
 import { SettingsTooltip } from './settingsTooltip';
 
 export const AvatarWithSettings = () => {
-  const { user } = useAuthContext();
   const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+  const avatarRef = useRef(null);
+
+  const { user } = useAuthContext();
+  useOutsideClick(avatarRef, () => setIsTooltipOpen(false));
 
   if (!user) {
     return null;
   }
 
   return (
-    <AvatarContainer>
+    <AvatarContainer ref={avatarRef}>
       <SettingsButton onClick={() => setIsTooltipOpen((isOpen) => !isOpen)}>
         <Avatar src={user?.avatar} alt="user avatar" />
         <ChevronDownSVG />
