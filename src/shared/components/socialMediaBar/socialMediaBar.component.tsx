0af796b4197c3342059687cdf3726379e18131ff
Wojciech Florczak <florczak@wojciech.dev>
2023-04-06 09:47:39 +0200

    [TRA-308] add hover state for social media icons (#142)

--- src/shared/components/socialMediaBar/socialMediaBar.component.tsx
+++ src/shared/components/socialMediaBar/socialMediaBar.component.tsx
@@ -3,26 +3,36 @@ import { ReactComponent as FbSVG } from '../../../images/icons/social-media-fb.s
 import { ReactComponent as GithubSVG } from '../../../images/icons/social-media-github.svg';
 import { ReactComponent as InstagramSVG } from '../../../images/icons/social-media-instagram.svg';
 import { ReactComponent as LinkedinSVG } from '../../../images/icons/social-media-linkedin.svg';
-import { Container } from './socialMediaBar.styles';
+import { Container, SocialMediaLink, SvgGradient } from './socialMediaBar.styles';
+
+const SvgBackground = () => (
+  <SvgGradient aria-hidden="true" focusable="false">
+    <linearGradient id="link-hover-gradient" x2="1" y2="1" gradientTransform="rotate(-50)">
+      <stop offset="0%" stopColor="#FFDE00" stopOpacity={0.8} />
+      <stop offset="100%" stopColor="#42F272" stopOpacity={0.8} />
+    </linearGradient>
+  </SvgGradient>
+);
 
 export const SocialMediaBar = () => {
   return (
     <Container>
-      <a href="https://github.com/apptension" target="_blank" rel="noreferrer">
+      <SocialMediaLink href="https://github.com/apptension">
         <GithubSVG />
-      </a>
-      <a href="https://www.behance.net/apptension" target="_blank" rel="noreferrer">
+      </SocialMediaLink>
+      <SocialMediaLink href="https://www.behance.net/apptension">
         <BehanceSVG />
-      </a>
-      <a href="https://www.instagram.com/apptension" target="_blank" rel="noreferrer">
+      </SocialMediaLink>
+      <SocialMediaLink href="https://www.instagram.com/apptension">
         <InstagramSVG />
-      </a>
-      <a href="https://www.facebook.com/apptension" target="_blank" rel="noreferrer">
+      </SocialMediaLink>
+      <SocialMediaLink href="https://www.facebook.com/apptension">
         <FbSVG />
-      </a>
-      <a href="https://linkedin.com/company/apptension" target="_blank" rel="noreferrer">
+      </SocialMediaLink>
+      <SocialMediaLink href="https://linkedin.com/company/apptension">
         <LinkedinSVG />
-      </a>
+      </SocialMediaLink>
+      <SvgBackground />
     </Container>
   );
 };
