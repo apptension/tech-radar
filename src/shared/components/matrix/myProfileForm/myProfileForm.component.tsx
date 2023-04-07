9281e17a42552cd7296bfe95b95e2af849e058b2
Wojciech Florczak <florczak@wojciech.dev>
2023-04-07 09:03:10 +0200

    Feature/tra 307 add button to profile form (#143)
    [TRA-307] add go back button to profile form

--- src/shared/components/matrix/myProfileForm/myProfileForm.component.tsx
+++ src/shared/components/matrix/myProfileForm/myProfileForm.component.tsx
@@ -1,13 +1,20 @@
 import { Controller } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { ROUTES } from '../../../../routes/app.constants';
 import { VALIDATION_MESSAGES } from '../../../utils/validationMessages';
+import { Button } from '../../button';
+import { ButtonSize, ButtonVariant, ButtonIcon } from '../../button/button.types';
 import { MatrixSelectField } from '../../fields/matrixSelectField';
 import { MatrixTextField } from '../../fields/matrixTextField';
 import { Loader } from '../../loader';
 import { FormActions } from '../formActions';
 import { FieldContainer, Form } from '../personalInfoForm/personalInfoForm.styles';
+import { ButtonsContainer } from './myProfileForm.styles';
 import { useMyProfileForm } from './useMyProfileForm.hook';
 
 export const MyProfileForm = () => {
+  const history = useHistory();
+
   const {
     form: {
       control,
@@ -90,7 +97,19 @@ export const MyProfileForm = () => {
           )}
         />
       </FieldContainer>
-      <FormActions withoutBackButton nextLabel="Save" isDisabled={!isDirty} isLoading={isSubmitting} />
+      <ButtonsContainer>
+        <FormActions withoutBackButton nextLabel="Save" isDisabled={!isDirty} isLoading={isSubmitting} />
+        <Button
+          size={ButtonSize.LARGE}
+          variant={ButtonVariant.PRIMARY}
+          type="button"
+          disabled={isSubmitting}
+          onClick={() => history.push(ROUTES.matrix)}
+          icon={ButtonIcon.ARROW}
+        >
+          Go to skills matrix
+        </Button>
+      </ButtonsContainer>
     </Form>
   );
 };
---
+++ src/shared/components/matrix/myProfileForm/myProfileForm.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  flex-wrap: wrap;
+  gap: 40px;
+`;
