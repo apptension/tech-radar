b32ac9990454db32c6ec252b691647d3eb8df7fa
Wojciech Florczak <wflorczak@apptension.com>
2023-05-26 10:58:43 +0200

    [TRA-386] move firebase credentials to env (#184)

--- src/shared/services/firebase.ts
+++ src/shared/services/firebase.ts
@@ -1,13 +1,22 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAMKm7Zv0ZZoiONMfScv8RMZjn4Hy7wTzE',
-  authDomain: 'apptension-tech-radar.firebaseapp.com',
-  projectId: 'apptension-tech-radar',
-  storageBucket: 'apptension-tech-radar.appspot.com',
-  messagingSenderId: '387773266107',
-  appId: '1:387773266107:web:520eadf4697074e2f8119e',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
